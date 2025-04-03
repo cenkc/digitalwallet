ddbf485df80ef316153f823420cbdf8d276858dc
Cenk CANARSLAN <cenk.canarslan@gmail.com>
2025-04-03 04:46:24 +0300

    clean-up

--- src/main/java/com/cenkc/digitalwallet/service/UserService.java
+++ src/main/java/com/cenkc/digitalwallet/service/UserService.java
@@ -8,12 +8,4 @@ public interface UserService {
 
     User getCurrentUser();
     UserResponseDTO addUser(UserRequestDTO userRequestDTO);
-//    User findByUsername(String username);
-//    User findByEmail(String email);
-//    User findByTckn(String tckn);
-//    User save(User user);
-//    void delete(Long id);
-//    List<User> findAll();
-//    List<User> findAllActive();
-//    List<User> findAllDeleted();{
 }
